e56b2ca8593c16ba89dba3fd2dc6981407aedb30
zaky.hatmoko <zaky.hatmoko@tokopedia.com>
2022-10-27 18:50:00 +0700

    finished test6

--- src/routes/Test4/Table.js
+++ src/routes/Test4/Table.js
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { DataContext } from "./DataContext";
+import { useContext } from 'react'
+import { DataContext } from '../../common/DataContext'
 
 const Table = () => {
   return (
@@ -10,27 +10,25 @@ const Table = () => {
           <th>Age</th>
           <th>Address</th>
         </tr>
-        <TableContent/>
+        <TableContent />
       </thead>
     </table>
   )
 }
 
 const TableContent = () => {
-  const {data} = useContext(DataContext)
+  const { data } = useContext(DataContext)
   const isDataExist = data.length > 0
 
   if (!isDataExist) return <tr>User does not exist</tr>
 
-  return (
-    data.map(d => (
-      <tr key={`${d.name}-${d.address}-${d.age}`}>
-        <td>{d.name}</td>
-        <td>{d.age}</td>
-        <td>{d.address}</td>
-      </tr>
-    ))
-  )
+  return data.map((d) => (
+    <tr key={`${d.name}-${d.address}-${d.age}`}>
+      <td>{d.name}</td>
+      <td>{d.age}</td>
+      <td>{d.address}</td>
+    </tr>
+  ))
 }
 
-export default Table;
+export default Table
