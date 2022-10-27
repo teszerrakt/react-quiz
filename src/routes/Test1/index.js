c62d62111bf9ce34efb11b91fb3105710382a787
zaky.hatmoko <zaky.hatmoko@tokopedia.com>
2022-10-27 10:47:18 +0700

    finished test1

--- src/routes/Test1/index.js
+++ src/routes/Test1/index.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
+
 const Test1 = () => {
+  const [input, setInput] = useState('')
+
+  const handleChange = (event) => {
+    setInput(event.target.value)
+  }
+
   return(
     <div>
       <ul>
         <li>Please render the inputted value</li>
       </ul>
       <p>
-        <b>User Input: </b> ______
+        <b>User Input: </b> {input}
       </p>
-      <input type="text" placeholder="input here"/>
+      <input type="text" placeholder="input here" value={input} onChange={handleChange}/>
     </div>
   )
 }
